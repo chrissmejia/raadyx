cd5a5b8ed6628a417cecc3456858df453a8a1871
Chriss Mejía <me@chrissmejia.com>
2018-03-30 10:52:17 -0600

    draft 1

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+jQuery(function ($) {
+
+    'use strict';
+    // --------------------------------------------------------------------
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('a.page-scroll').on('click', function (e) {
+            e.preventDefault();
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // Closes the Responsive Menu on Menu Item Click
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('.navbar-collapse ul li a').on('click', function () {
+            if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+                $('.navbar-toggle:visible').trigger('click');
+            }
+        });
+    }());
+
+}); // JQuery end
