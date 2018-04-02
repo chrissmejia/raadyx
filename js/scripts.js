e7b5df2ecfeeea64d1c1347388e67b48523e28cb
Chriss Mejía <me@chrissmejia.com>
2018-04-02 15:25:26 -0600

    intership form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 jQuery(function ($) {
 
     'use strict';
+
+    // --------------------------------------------------------------------
+    // Apply form
+    // --------------------------------------------------------------------
+    (function () {
+        $('#contact').addClass("js");
+
+        $('#apply').on('click', function (e) {
+            e.preventDefault();
+            $('#contact').toggleClass("show");
+        });
+
+        $('#sendmail').on('click', function (e) {
+            e.preventDefault();
+            document.location.href = "mailto:internship@raadyx.com?Subject=Application&body=Name: " +
+                $('#firstnameInput').val() + " " + $('#lastnameInput').val() + "%0D%0AEmail: " +
+                $('#emailInput').val() + "%0D%0APhone: " +
+                $('#phoneInput').val() + "%0D%0ASex: " +
+                $('#sexInput').val() + "%0D%0ACountry: " +
+                $('#countryInput').val() + "%0D%0AUniversity: " +
+                $('#universityInput').val() + "%0D%0ADate of birth: " +
+                $('#monthInput').val() + "/" + $('#dayInput').val() + "/" + $('#monthInput').val() + "%0D%0AIntroduce yourself: " +
+                $('#otherText').val();
+            $('#contact').addClass("sent");
+        });
+    }());
+
     // --------------------------------------------------------------------
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     // --------------------------------------------------------------------
-
     (function () {
         $('a.page-scroll').on('click', function (e) {
             e.preventDefault();
@@ -18,7 +44,6 @@ jQuery(function ($) {
     // --------------------------------------------------------------------
     // Closes the Responsive Menu on Menu Item Click
     // --------------------------------------------------------------------
-
     (function () {
         $('.navbar-collapse ul li a').on('click', function () {
             if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
